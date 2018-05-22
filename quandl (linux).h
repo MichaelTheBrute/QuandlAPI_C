81c3cf288ef221223ed2603cda71ca57bc81e908
Michael Mackend <michaelmackend@outlook.com>
2018-05-22 15:53:33 -0700

    update this for v3 and ssl (courtesy of some random online guy)

--- quandl (linux).h
+++ quandl (linux).h
@@ -1,17 +1,19 @@
-/*
- * quandl.h
+/*  quandl(linux).h
  *
- *  Created on: 30/09/2013 (Updated on 10/09/2014)
+ *  Created on: 30/09/2013 (Updated on 10/09/2014) ((Revised on 15/08/2016))
  *      Author: Zhiwei Fu
+ *      Revision: Jose Marqués Fernández    http://www.epigijon.uniovi.es/
  */
 
 /* This programme is free software. It is developed by Dr Zhiwei Fu as a product
    contributing to quandl.com.
-
    This programme is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY, without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
+
+//OpenSSL required, for install on ubuntu: sudo apt-get install libssl-dev  
+//for compile: g++ xxx.cpp -std=c++11 -lssl -lcrypto -o xxx
 
 #ifndef QUANDL_H_
 #define QUANDL_H_
@@ -26,220 +28,240 @@
 #include <netinet/tcp.h> // for TCP_NODELAY
 #include <time.h> // time.h
 
-using namespace std;
+// for https
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <stdarg.h>
+
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+//using namespace std;
 
 class quandl {
-	public:
-		quandl(){};
-		~quandl(){};
-		// To store the token in "AuthCode", which is a public variable in the class.
-		void auth(string code){
-			AuthCode = code;
-		}
+    public:
 
-		//To download file from the website defined by the first 
-		//argument.
-		// To determine the website address by the token stored in "code"
-		// and call the function "download"
-		void get(string code){
-			//Set optional variables by default
-			string order = "asc"; 
-			string type = "csv";
+        quandl(){
 
-			string website = "https://www.quandl.com/api/v1/datasets/" + code 
-				+ "." + type + "?sort_order=" + order;
-			if(AuthCode.length() == 0){
-				cout << "It would appear you are\'nt using an authentication"
-				  << " token. Please visit https://www.quandl.com/help/c++"
-				  << " or your usage may be limited.\n";
-			}
-			else{
-				website += "&auth_token=" + AuthCode;
-			}
 
-			string FileName;
-			int iLength = code.length();
-			for (int i=0; i < iLength; i++){
-				if(code.substr(i, 1) == "/"){
-					FileName = code.substr(i+1, iLength - i -1);
-					break;
-				}
-			}
+                };
+        ~quandl(){};
+        // To store the token in "AuthCode", which is a public variable in the class.
+        void auth(std::string code){
+            AuthCode = code;
+        }
 
-			download(website, FileName, type);
-			return;
-		}
+        //To download file from the website defined by the first 
+        //argument.
+        // To determine the website address by the token stored in "code"
+        // and call the function "download"
+        void get(std::string code){
+            //Set optional variables by default
+            std::string order = "asc"; 
+            std::string type = "json";
 
-		// All parameters are prescribed by users.
-		// 1. Quandl code;
-		// 2. Ascending/descending order;
-		// 3. Start date;
-		// 4. End date;
-		// 5. Transformation;
-		// 6. collapse;
-		// 7. Rows;
-		// 8. Output type
-		// There are 7 optional arguments compared to the one above.
-		void get(string code, string order, string StartDate, string EndDate,
-			string transformation, string collapse, string rows, string type){
+            std::string website = "https://www.quandl.com/api/v3/datasets/" + code 
+                + "." + type + "?sort_order=" + order;
+            if(AuthCode.length() == 0){
+                std::cout << "It would appear you are\'nt using an authentication"
+                  << " token. Please visit https://www.quandl.com/help/c++"
+                  << " or your usage may be limited.\n";
+            }
+            else{
+                website += "&api_key=" + AuthCode;
+            }
 
-			string website = "https://www.quandl.com/api/v1/datasets/" + code 
-				+ "." + type + "?sort_order=" + order;
-			if(AuthCode.length() == 0){
-				cout << "It appear you are\'nt using an authentication"
-				  << " token. Please visit https://www.quandl.com/help/api for getting one"
-				  << " ; otherwise your usage may be limited.\n";
-			}
-			else{
-				website += "&auth_token=" + AuthCode;
-			}
+            std::string FileName;
+            int iLength = code.length();
+            for (int i=0; i < iLength; i++){
+                if(code.substr(i, 1) == "/"){
+                    FileName = code.substr(i+1, iLength - i -1);
+                    break;
+                }
+            }
 
-			website += "&trim_start=" + StartDate;
-			website += "&trim_end=" + EndDate;
-			website += "&transformation=" + transformation;
-			website += "&collapse=" + collapse;
-			website += "&rows=" + rows;
+            download(website, FileName, type);
+            return;
+        }
 
-			string FileName;
-			int iLength = code.length();
-			for (int i=0; i < iLength; i++){
-				if(code.substr(i, 1) == "/"){
-					FileName = code.substr(i+1, iLength - i -1);
-					break;
-				}
-			}
+        // All parameters are prescribed by users.
+        // 1. Quandl code;
+        // 2. Ascending/descending order;
+        // 3. Start date;
+        // 4. End date;
+        // 5. Transformation;
+        // 6. collapse;
+        // 7. Rows;
+        // 8. Output type
+        // There are 7 optional arguments compared to the one above.
+        void get(std::string code, std::string order, std::string StartDate, std::string EndDate,
+            std::string transformation, std::string collapse, std::string rows, std::string type){
 
-			download(website, FileName, type);
-		}
-	private: 
-		string AuthCode;
-		// This void function "download" is to download a file from internet.
-		// The "download" function is developed with a general purpose of downloading
-		// files from internet. It is based on the socket programming.
-		void download(string website, string FileName, string FileType){
-			struct sockaddr_in ServAddr;
-			struct hostent *ServInf;
-			int sockfd;
-			int iMessage = 1048576; //1 MB for the buff storage
-			char message[iMessage];
-			string request = "GET ";
-			ofstream fid;
-			int iStart, iEnd, iLength;
+            std::string website = "https://www.quandl.com/api/v3/datasets/" + code 
+                + "." + type + "?sort_order=" + order;
+            if(AuthCode.length() == 0){
+                std::cout << "It appear you are\'nt using an authentication"
+                  << " token. Please visit https://www.quandl.com/help/api for getting one"
+                  << " ; otherwise your usage may be limited.\n";
+            }
+            else{
+                website += "&api_key=" + AuthCode;
+            }
 
-			//Get a socket
-			if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-				perror("   *** Error when calling socket()! ***\n");
-				return;
-			}
+            website += "&trim_start=" + StartDate;
+            website += "&trim_end=" + EndDate;
+            website += "&transformation=" + transformation;
+            website += "&collapse=" + collapse;
+            website += "&rows=" + rows;
 
-			//To define the server's address.
-			memset(&ServAddr, 0, sizeof(ServAddr));
-			string host;
-			iLength = website.length();
-			iStart = 0; iEnd = iLength - 1;
-			if(website.substr(0,8) == "https://") iStart = 8;
-			else if(website.substr(0,7) == "http://") iStart = 7;
-			for(int i = iStart; i<iLength; i++){
-			  if(website.substr(i, 1)=="/"){
-				iEnd = i - 1;
-				break;}
-			}
-			host = website.substr(iStart, iEnd - iStart + 1);
-			ServInf = gethostbyname(host.c_str());
-			if(ServInf == NULL){
-				perror("   *** The hostname is not recognized! ***\n");
-				return;
-			}
-			memcpy((char *)&ServAddr.sin_addr.s_addr, (char *)ServInf->h_addr,
-				   ServInf->h_length);
-			ServAddr.sin_family = AF_INET;
-			ServAddr.sin_port = htons(80);
+            std::string FileName;
+            int iLength = code.length();
+            for (int i=0; i < iLength; i++){
+                if(code.substr(i, 1) == "/"){
+                    FileName = code.substr(i+1, iLength - i -1);
+                    break;
+                }
+            }
 
-			// To set the socket with options.
-			int BDP =100*1024;
-			int ret = 0;
-		/*	//int flags = fcntl(sockfd, F_GETFL, 0);
-			//ret = fcntl( sockfd, F_SETFL, O_NONBLOCK); //must MSG_DONTWAIT, 
-			//     &~O_NONBLOCK
-			if (ret != 0) {
-	  			printf("Couldn't setsockopt(O_NONBLOCK)\n");
-				return; // ?exit(-1);
-			}
-			//setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *) &iTime, 
-			//sizeof(iTime)); //work for non-blocking only
-			//int nZero=0;
-			//setsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, (char *)&nZero, 
-			//sizeof(nZero));
-		*/
-			ret = setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, (char *)&BDP, 
-				  sizeof(BDP));
-			if (ret == -1) {
-				  printf("   *** Couldn't setsockopt(SO_RCVBUF)! ***\n");
-				  return;
-			}
-			int on = 1;
-			ret = setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on));
-			if (ret == -1) {
-				  printf("   *** Couldn't setsockopt(TCP_NODELAY)! ***\n");
-				  return;
-			}
+            download(website, FileName, type);
+        }
+    private: 
+        std::string AuthCode;
+        // This void function "download" is to download a file from internet.
+        // The "download" function is developed with a general purpose of downloading
+        // files from internet. It is based on the socket programming.
+        void download(std::string website, std::string FileName, std::string FileType){
 
-			// To connect to the server.
-			ret = connect(sockfd, (struct sockaddr *)&ServAddr, sizeof(ServAddr));
-			if(ret == -1){
-				perror("*** Connection fails! ***\n");
-				return;
-			}
+                    struct sockaddr_in ServAddr;
+                    struct hostent *ServInf;
+                    int sockfd;
+                    int iMessage = 1048576; //1 MB for the buff storage
+                    char message[iMessage];
+                    std::string request = "GET ";
+                    std::ofstream fid;
+                    int iStart, iEnd, iLength;
 
-			// To send the request.
-			request += website + " HTTP/1.1\r\nHost: "
-				+ host + "\r\nConnection: close\r\n\r\n";
-			//write(sockfd,request.c_str(),request.length());
-			send(sockfd, request.c_str(), request.length(), 0);
+                    SSL_CTX *ctx;
+                    SSL *ssl;                 
+                    OpenSSL_add_all_algorithms();
+                    ERR_load_crypto_strings();
+                    SSL_load_error_strings();
 
-			// To initiate the file.
-			fid.open((FileName + "." + FileType).c_str());
-			if(fid.fail()){
-				   cerr << "*** Error when openning file! ***\n" << endl;
-				   return;
-       			}
+                    if(SSL_library_init() < 0){
+                        std::cerr<<"OpenSSL error"<<std::endl;
+                        return;
+                    }
 
-			// To read dataflow for the file.
-			memset(&message, 0, iMessage);
-			int iRecv = 1; iStart = 1;
-			//string TempStr;
-			char tChar[1];
-			int nLoop = 0;
-			while (iRecv != 0 && iRecv != -1){
-				nLoop++;
-				iRecv = recv(sockfd, message, iMessage, MSG_WAITALL);
-				if (nLoop == 1) {
-					string TempStr;
-					TempStr = string(message);
-					string::size_type iStart = TempStr.find("\r\n\r\n");
-					if (iStart < iRecv - 4) {
-						int nLenth = iRecv;
-						TempStr = TempStr.substr(iStart + 4, nLenth - (iStart + 4));
-						nLenth = TempStr.length();
-						fid.write(TempStr.c_str(), nLenth);
-					}
-					else {
-						fid.write(message, iRecv);
-					}
-				}
-				else {
-					fid.write(message, iRecv);
-				}
-			}
+                    ctx = SSL_CTX_new (SSLv23_client_method ());
+                    SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);
 
-				// To close the downloaded file
-			fid.close();
-			if(iRecv == -1){
-				perror("*** Error when calling recv()! ***\n");
-				return;
-			}
-			shutdown(sockfd, 0);
-		}
+                    //To define the server's address.
+                    memset(&ServAddr, 0, sizeof(ServAddr));
+                    std::string host;
+                    iLength = website.length();
+                    iStart = 0; iEnd = iLength - 1;
+                    if(website.substr(0,8) == "https://") iStart = 8;
+                    else if(website.substr(0,7) == "http://") iStart = 7;
+                    for(int i = iStart; i<iLength; i++){
+                        if(website.substr(i, 1)=="/"){
+                            iEnd = i - 1;
+                            break;
+                        }
+                    }
+                    host = website.substr(iStart, iEnd - iStart + 1);
+
+                    ServInf = gethostbyname(host.c_str());
+                    if(ServInf == NULL){
+                        std::cerr<<"HostName error"<<std::endl;
+                        return;
+                    }
+
+                    //Get a socket
+                    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+                        std::cerr<<"Socket error"<<std::endl;
+                        return;
+                    }
+
+                    memset(&ServAddr, 0, sizeof(ServAddr));
+                    ServAddr.sin_family = AF_INET;
+                    ServAddr.sin_port = htons(443);
+                    ServAddr.sin_addr.s_addr = *(long*)(ServInf->h_addr);
+
+                    if ( connect(sockfd, (struct sockaddr*)&ServAddr, sizeof(ServAddr)) == -1 ) {
+                       std::cerr<<"Conection error"<<std::endl;
+                       return;
+                    }
+
+                    ssl = SSL_new(ctx); 
+                    SSL_set_fd(ssl, sockfd);
+
+                    SSL_connect(ssl);
+
+                    // To send the request.
+                    request += website + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n";                    
+                    SSL_write(ssl, request.c_str(), request.length());
+
+                    // To initiate the file.
+                    fid.open((FileName + "." + FileType).c_str());
+                    if(fid.fail()){
+                        std::cerr << "OpeningFile error" << std::endl;
+                        return;
+                    }
+
+                    // To read dataflow for the file.
+                    int iRecv = 1;                    
+                    std::string RcvDt;
+
+                    while (iRecv != 0 && iRecv != -1){
+                        memset(message, '\0', sizeof(message));
+                       iRecv= SSL_read(ssl, message, iMessage);
+                        RcvDt+=std::string(message);
+
+                    }
+                    std::string initS="\r\n\r\n";
+                    std::string servR="\r\n";
+
+                    std::size_t found = RcvDt.find(initS);
+                    if (found!=std::string::npos){
+                        RcvDt.erase(0,found+10);
+                    }
+                    while((found!=std::string::npos)&&(found<RcvDt.size()-8)){
+                        found = RcvDt.find(servR);
+                        if(found<(RcvDt.size()-8)){
+                            RcvDt.erase(found,8);
+                        }
+                    }
+                    fid.write(RcvDt.c_str(), RcvDt.size());
+
+
+                    // To close the downloaded file
+                    fid.close();
+                    if(iRecv == -1){
+                        std::cerr<<"SSL_read error"<<std::endl;
+                        return;
+                    }
+
+
+                    SSL_free(ssl);
+                    close(sockfd);
+                    SSL_CTX_free(ctx);
+
+
+
+
+        }
 };
 
 #endif /* QUANDL_H_ */
